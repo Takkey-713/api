14a0f363778ed74daa93e9e74a14ade65610c813
Takkey-713 <takiyu713@gmail.com>
2021-03-09 15:39:34 +0900

    fix auth_controller.rb

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,15 +2,9 @@ class AuthController < ApplicationController
 
   def sign_up
     user = User.new(registration_params)
-    binding.pry
-    if user.save
-      session[:user_id] = user.id
-      payload = {logged_in: true, user: user}
-    else
-      payload = {logged_in: false, message: "認証に失敗しました。"}
-    end
-     render json: payload
-end
+    payload = check_signup(user)
+    render json: payload
+  end
 
   def sign_in
     user = User.find_by(email: params[:user][:email].downcase)
@@ -41,5 +35,18 @@ end
   private
   def registration_params
     params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  def check_signup(user)
+    if user.valid?
+      user.save!
+      session[:user_id] = user.id
+      payload = {logged_in: true, user: user}
+    elsif user.errors.full_messages.include?("Email has already been taken")
+      payload = {logged_in: false, errors: "入力されたメールアドレスは既に登録されています。"}
+    else
+      binding.pry
+      payload = {logged_in: false, errors: "認証に失敗しました。"}
+    end
   end
 end
