1b4765729b011abd516f7707853f5e0c58e5f577
Takkey-713 <takiyu713@gmail.com>
2021-03-10 17:00:27 +0900

    fix cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    Rails.ENV == "development"  ? origins 'localhost:3000' : origins '52.192.7.231'
+    # いじるポイント
 
     resource '*',
       headers: :any,
