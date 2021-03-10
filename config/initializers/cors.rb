89122bae314cfe46a4c59db5032b4b89b65c9a5a
Takkey-713 <takiyu713@gmail.com>
2021-03-10 17:18:55 +0900

    fix cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    Rails.ENV == "development"  ? origins 'localhost:3000' : origins '52.192.7.231'
-    # いじるポイント
-
+    if Rails.env.production?
+      origins '52.192.7.231'
+    else 
+      origins 'localhost:3000'
+    end
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
