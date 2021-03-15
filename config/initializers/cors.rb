6cbdac2c745d852d875972ec46a0a9f56689597b
Takkey-713 <takiyu713@gmail.com>
2021-03-15 20:35:47 +0900

    add cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    Rails.env.production? ? (origins 'http://52.192.7.231') : (origins 'http://localhost:3000')
+    Rails.env.production? ? (origins 'http://52.192.7.231' || 'http://52.192.7.231/main') : (origins 'http://localhost:3000')
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
